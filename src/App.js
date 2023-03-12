141bc506ec6aacc8a0ba205edfcf7c788532b556
oleksandr aleksashyn <oleksandr.aleksashyn@delasport.com>
2023-03-12 19:14:16 +0200

    Homework #28: Attempt #1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Button from "./components/Button";
 
 function App() {
+  let [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{count}</p>
+
+      <Button onClick={increase}>Increase</Button>
+      <Button onClick={decrease}>Decrease</Button>
     </div>
   );
 }
